e63d3ae808e653b8c356c76908a79b9ea23f63bc
Marcio Mazza <marciomazza@gmail.com>
2013-07-01 17:32:05 -0300

    Relocate optional http_proxy to site_properties

--- src/collective.conference/collective/conference/browser/apybcheck.py
+++ src/collective.conference/collective/conference/browser/apybcheck.py
@@ -1,16 +1,21 @@
 import json
 import urllib2
+
+from Products.CMFCore.utils import getToolByName
 from zope.app.component.hooks import getSite
+
 
 def check_apyb_membership(email):
     # XXX refactor the proxy config to run only once
 
     # If your site is behind a proxy,
     # you must configure a 'http_proxy' property for it
-    #  (in its "Properties" tab in the ZMI)
-    portal = getSite()
-    if hasattr(portal, 'http_proxy'):
-        proxy = urllib2.ProxyHandler({'http': portal.http_proxy})
+    # in "portal_properties/site_properties"
+    portal_properties = getToolByName(getSite(), 'portal_properties')
+    site_properties = portal_properties['site_properties']
+    http_proxy = site_properties.getProperty('http_proxy')
+    if http_proxy:
+        proxy = urllib2.ProxyHandler({'http': http_proxy})
         opener = urllib2.build_opener(proxy)
         urllib2.install_opener(opener)
 
